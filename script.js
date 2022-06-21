99bcf1f20ef412b42c1b63e06dd9594322497d76
Barakat Oseni <barakatoseni93@gmail.com>
2022-06-21 08:49:16 +0100

    edited

---
+++ script.js
@@ -0,0 +1,14 @@
+var name
+
+var age
+
+var country
+
+
+name=prompt("enter your name :","")
+
+age=prompt("enter your age :","")
+
+country=prompt("enter your country :","")
+
+document.write(name+ " is " +age+ " years old " +country+ " citizen ")
